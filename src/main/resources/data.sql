7b3c318de0cd87773ed16d9289f16a6d3f2c1594
Ficko05 <filip-f@hotmail.com>
2021-09-22 19:41:03 +0200

    db med dummy data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-insert into STUDENT(id, name, mail) values(101, 'Alice', 'al@mail.dk');
-insert into STUDENT(id, name, mail) values(102, 'Bob', 'bo@mail.dk');
-insert into STUDENT(id, name, mail) values(103, 'Caroline', 'ca@mail.dk');
-insert into STUDENT(id, name, mail) values(104, 'Daniel', 'da@mail.dk');
+insert into student(id, name, mail) values(101, 'Alice', 'al@mail.dk');
+insert into student(id, name, mail) values(102, 'Bob', 'bo@mail.dk');
+insert into student(id, name, mail) values(103, 'Caroline', 'ca@mail.dk');
+insert into student(id, name, mail) values(104, 'Daniel', 'da@mail.dk');
 
 
