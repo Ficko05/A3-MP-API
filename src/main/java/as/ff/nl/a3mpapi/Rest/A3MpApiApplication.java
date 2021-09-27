a9ea7fe4ce8accfaa970674ce2c5824b41ec4b90
AllanSimonsen789 <allanalli@hotmail.com>
2021-09-27 15:56:34 +0200

    failed attempt to try and make grpc run together with rest, added more sql statements

--- src/main/java/as/ff/nl/a3mpapi/Rest/A3MpApiApplication.java
+++ src/main/java/as/ff/nl/a3mpapi/Rest/A3MpApiApplication.java
@@ -1,13 +1,12 @@
 package as.ff.nl.a3mpapi.Rest;
 
+import as.ff.nl.a3mpapi.grpc.server.StudentServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class A3MpApiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(A3MpApiApplication.class, args);
-    }
+    public static void main(String[] args) {SpringApplication.run(A3MpApiApplication.class, args);}
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,12 @@ insert into student(student_id, name, mail) values(101, 'Alice', 'al@mail.dk');
 insert into student(student_id, name, mail) values(102, 'Bob', 'bo@mail.dk');
 insert into student(student_id, name, mail) values(103, 'Caroline', 'ca@mail.dk');
 insert into student(student_id, name, mail) values(104, 'Daniel', 'da@mail.dk');
+insert into student(student_id, name, mail) values(105, 'Allan', 'al@mail.dk');
+insert into student(student_id, name, mail) values(106, 'Nina', 'nl@mail.dk');
+insert into student(student_id, name, mail) values(107, 'Filip', 'fl@mail.dk');
+insert into student(student_id, name, mail) values(108, 'Dora', 'do@mail.dk');
+insert into student(student_id, name, mail) values(109, 'Steffen', 'st@mail.dk');
+
 
 
 insert into subject(subject_id, name, classroom) values(1, 'System Integration', '2.62');
@@ -13,3 +19,24 @@ insert into subject_students values(1, 101);
 insert into subject_students values(1, 102);
 insert into subject_students values(1, 103);
 insert into subject_students values(1, 104);
+insert into subject_students values(1, 105);
+insert into subject_students values(1, 106);
+insert into subject_students values(1, 107);
+insert into subject_students values(1, 108);
+
+insert into subject_students values(2, 101);
+insert into subject_students values(2, 102);
+insert into subject_students values(2, 103);
+insert into subject_students values(2, 104);
+insert into subject_students values(2, 105);
+insert into subject_students values(2, 106);
+insert into subject_students values(2, 107);
+
+insert into subject_students values(3, 101);
+insert into subject_students values(3, 102);
+insert into subject_students values(3, 103);
+insert into subject_students values(3, 104);
+insert into subject_students values(3, 105);
+insert into subject_students values(3, 106);
+insert into subject_students values(3, 107);
+insert into subject_students values(3, 109);
